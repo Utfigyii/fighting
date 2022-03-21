ae09eeeacbf784d485648cd3dd3fc744d1700b46
Utfigyii <42611989+Utfigyii@users.noreply.github.com>
2022-03-21 12:58:31 +0200

    something

---
+++ assets/scripts/FIGHTER_stickman.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+
+var velocity = Vector2()
+
+var isOnGround  = true
+var isMoving = false
+var canAttack = true
+var isStunned = false
+
+func _physics_process(delta):
+	
+	pass
+	
+	
+func _getInputDirection():
+	var inputDirection = Vector2()
+	inputDirection.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_right")
+	inputDirection.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	return inputDirection
